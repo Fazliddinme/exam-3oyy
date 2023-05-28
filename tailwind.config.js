1ae50eeef127532ee5739400838325504c747b89
Fazliddinme <xushfazliddin2gmail.com>
2023-05-29 00:47:20 +0500

    salom

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    extend: {
+      colors: {
+        firstColor: '#FFFFFF';
+        secondColor: '#CCCCCC';
+        thirdColor: '#808080';
+        fourth: '#1C3554';
+        fifthColor: '#F72585';
+        sixthColor: '#021934';
+        seventhColor: '#AAAAAA';
+        eighthColor: '##4586D6';
+        ninethColor: '#000000';
+      }
+    },
+  },
+  plugins: [],
+}
+
